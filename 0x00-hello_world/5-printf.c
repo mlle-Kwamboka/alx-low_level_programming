fb61307c64f24699bbd1332f4ccd1dc340be0640
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-03-10 16:24:06 -0800

    Using printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*main prints out exactly with proper grammar, but the outcome is a piece of art folloed by new line if exited properly, otherwise returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art\n");
+	return(0);
+}
+
