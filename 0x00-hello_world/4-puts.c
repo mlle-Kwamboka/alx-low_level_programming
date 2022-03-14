4451d2c36c7c4dd234d448a36b9382cd81cb79c6
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-03-14 02:42:01 -0700

    using puts keyword

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * prints exactly what is input followed by a new line
+ * prints exactly what is input followed by a space
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle,\n");
 	return (0);
-
 }
