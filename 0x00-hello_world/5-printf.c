069dc469163cc0567b3b7bb393ada467fe3dfa5f
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-03-14 02:55:58 -0700

    Using printf keyword

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-/*main prints out exactly with proper grammar, but the outcome is a piece of art folloed by new line if exited properly, otherwise returns 0
+/**
+ * main prints out exactly with proper grammar,
+ * but the outcome is a piece of art
+ * if exited properly
+ * otherwise returns 0
  */
 
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
 }
 
