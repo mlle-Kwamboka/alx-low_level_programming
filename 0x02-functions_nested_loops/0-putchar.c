18844efa621041dee02d833f1dead72c0a45856a
mlle-Kwamboka <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@github.com>
2022-03-15 13:47:07 -0700

    Added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Function - prints _putchar followedby a new line
+ * Return: always 0 when exited successfully
+ */
+
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+
+	return (0);
+}
