762bf1f0e3f974b5ba79454c9bad895e353ade74
mlle-Kwamboka <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@github.com>
2022-03-15 15:32:04 -0700

    Add task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints alphabet x10 followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char y;
+	
+	y = 'a';
+
+	while (y <= z)
+	{
+		char n;
+		
+		n = '1';
+		
+		while (n <= 10)
+		{
+			putchar (y);
+			y++;
+		}
+	}n++
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,15 @@
 /**
+ * Function prototype for 0-putchar.c
+ */
+int main(void);
+
+/**
  * Function protype for 1-alphabet.c
  */
 void print_alphabet(void);
+
+/**
+ * Function prototype for task 2-print_alphabet_x10.c
+ */
+void print_alphabet_x10(void);
 
