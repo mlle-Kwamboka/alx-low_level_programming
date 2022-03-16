57776ec62e42aa216e375641604b59f67635b79a
mlle-Kwamboka <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@github.com>
2022-03-16 13:46:05 -0700

    Add task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * add - adds two integers and returns the result
  * @a: int to be added to b
  * @b: int to be added to a
+ *
  * Return: sum of a and b
  */
 int add(int a, int b)
