4536418ab18742434af2f76aac1d8d6d42701c99
mlle-Kwamboka <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@github.com>
2022-03-16 14:15:30 -0700

    Add task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * Function - prints _putchar followedby a new line
+ * main - prints _putchar followed by a new line
  * Return: always 0 when exited successfully
  */
 
 int main(void)
-	/**
-	 * Main - Can be called without any parameter
-	 * Return: value of data type int
-	 */
 {
-	putchar ('_');
-	putchar ('p');
-	putchar ('u');
-	putchar ('t');
-	putchar ('c');
-	putchar ('h');
-	putchar ('a');
-	putchar ('r');
-	putchar ('\n');
-
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 	return (0);
 }
