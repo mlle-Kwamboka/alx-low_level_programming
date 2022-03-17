cb3e73a9cdcf2f010c986ae3df1b19cd3345200f
mlle-Kwamboka <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@github.com>
2022-03-17 16:17:41 -0700

    Add file 1 update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,10 +2,10 @@
 
 /**
  * _isdigit checks for a digit between 0 through 9
- * @c: character being checked
+ * @c: integer being checked
  * Return 1 if is digit 0 otherwise
  */
 int _isdigit(int c)
 {
-	return (c == 0 && c <= 9);
+	return (c == '0' && c <= '9');
 }
