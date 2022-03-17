28f56d2e82a0b3bcd2cdb9185d394df86d476310
mlle-Kwamboka <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@github.com>
2022-03-17 16:07:08 -0700

    Add 2 tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,6 +2,7 @@
 
 /**
  * _isupper checks for upper case letter
+ * @c: integer being checked
  * Return 1 if is uppercase 0 otherwise
  */
 int _isupper(int c)
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit checks for a digit between 0 through 9
+ * @c: character being checked
+ * Return 1 if is digit 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c == 0 && c <= 9);
+}
