71e0403367d18510b9277c1c61c59cf8cc3ddb8f
mlle-Kwamboka <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@github.com>
2022-03-17 17:52:29 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/school.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i, j;
+
+	while (j > 0 && j < 10)
+	{
+		while (i > 0 && i < 14)
+		
+		{
+			_putchar(i + '0');
+			i++;
+		}
+		j++;
+	}
+	_putchar('\n');
+}
