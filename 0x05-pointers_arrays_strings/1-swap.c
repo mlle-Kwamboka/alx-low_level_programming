54fe1159a5e626c0de5e3018f199984dc4f0eb34
mlle-Kwamboka <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@github.com>
2022-03-21 12:53:38 -0700

    Add task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps values of two integers
+ * @*p @*pp: integers being swapped
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *p, int *pp)
+{
+	*p = 42;
+	*pp = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 void modif_my_char(char *cc, char ccc);
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif
