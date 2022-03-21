4aede7ed36702cc0680904a32d4cb0fe72260b67
mlle-Kwamboka <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@github.com>
2022-03-21 12:27:07 -0700

    update task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,33 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * reset_to_98 - uses pointer to update value of integer to 98
+ * @m: pointer being used
  *
- * Return: Always 0
- */
-int main (void)
-{
-	int n;
-	int *p;
-
-	n = 402;
-	p = &n;
-	printf("n=%d\n", n);
-	reset_to_98(p);
-	printf("n=%d\n", n);
-	return (0);
-}
-
-/**
- * reset_to_98 - update value of m to 98
- * @m: pointer to integer to be set to 98
- *
- * Return: Nothing
+ * Retun: Nothing
  */
 
 void reset_to_98(int *m)
 {
 	*m = 98;
 }
-
