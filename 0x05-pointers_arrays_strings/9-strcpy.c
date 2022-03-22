ca356bd4e4adda46c2548d8ca1bb5dd82fb3ed69
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-03-23 00:09:43 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src, 
+ * _strcpy - copies the string pointed to by src,
  * including the terminating null byte (\0)
  * @dest: pointer
  * @src: string to be copied
