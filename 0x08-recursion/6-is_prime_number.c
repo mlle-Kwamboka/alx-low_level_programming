2d671b9adf2c6c8b3dcb5923c43da9d4d7942c95
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-03-30 14:50:24 -0700

    Add task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_prime_number - return 1 if number is prime and 0 otherwise
+ *
+ * return: 1 if prime 0 if not
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (i <= 1)
+		return (0);
+	for (i = 2; i < (n/2); i++)
+	{
+		if ((n % i) != 0)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
+}
