add0c5000b2fca4089cc186908644137f69e4d1b
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-03-30 02:42:52 -0700

    Addtask6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,19 @@
 
 /**
  * main - print single digit base 10 numbers from 0-9
+ *
  * Return: Always 0 if successful
  */
 int main(void)
 {
-	char y = '0';
+	int i;
 
-	while (y <= '9')
+	i = 0;
+
+	while (i <= 9)
 	{
-		putchar (y);
-		y++;
+		putchar(i + '0');
+		i++;
 	}
 	putchar('\n');
 	return (0);
