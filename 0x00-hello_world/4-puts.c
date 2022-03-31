77b3e1740ab1f2a368576cd0165f0051a3befdfc
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-03-31 13:33:06 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\0");
 	return (0);
 }
