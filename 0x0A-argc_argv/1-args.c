9e13796daecb1fffcc4baa222b5770b8d5cb6f4f
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-04-01 08:43:38 -0700

    Ch

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,16 +4,13 @@
  * main - prints number of args passed
  * @argc: argument count
  * @argv: array of arguments
+ *
+ * Return: Always 0
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
+	printf("%d\n", argc - 1);
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-			printf("%d", i);
-	}
 	return (0);
 }
 
