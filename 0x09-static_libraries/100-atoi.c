6d8511033a68f455107fa6830fce8420491a791f
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-04-02 16:27:15 -0700

    Add some task files

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: The string that is being converted
+ *
+ * Return: An integer, o if there is no number
+ */
+int _atoi(char *s)
+{
+
+}
