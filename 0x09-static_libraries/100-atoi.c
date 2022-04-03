39deb88265129e0409b5748ec2d9d0f77bddc77b
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-04-02 18:38:23 -0700

    Add file 2

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,12 +1,3 @@
 #include "main.h"
 
-/**
- * _atoi - converts a string to an integer
- * @s: The string that is being converted
- *
- * Return: An integer, o if there is no number
- */
-int _atoi(char *s)
-{
 
-}
