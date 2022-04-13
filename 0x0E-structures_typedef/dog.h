c3c04daab7ca917b9b1159fb4317e1aeda388815
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-04-13 08:38:17 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
 * struct dog - My structure
 * @name: First member
@@ -12,10 +14,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
-#ifndef DOG_H
-#define DOG_H
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
