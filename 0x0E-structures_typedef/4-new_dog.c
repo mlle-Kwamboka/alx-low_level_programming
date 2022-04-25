568a33b95362e0c4d54a72fa294b89174209d811
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-04-25 15:42:37 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -73,7 +73,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (my_dog->name == NULL)
 	{
-		free(dog);
+		free(my_dog);
 		return (NULL);
 	}
 	my_dog->owner = malloc(sizeof(char) * (len2 + 1));
