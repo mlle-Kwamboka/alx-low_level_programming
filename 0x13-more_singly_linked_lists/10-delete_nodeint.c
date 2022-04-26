b7379160c7c44fe324de31907fff9b654e1e21e9
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-04-26 21:34:51 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,45 +1,40 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
  *
- * Return: pointer to the new node, or NULL
+ * Return: 1 (Success), or -1 (Fail)
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *new;
 	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
-		return (NULL);
+	if (*head == NULL)
+		return (-1);
 
-	new->n = n;
-	new->next = NULL;
-
-	if (idx == 0)
+	if (index == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		*head = (*head)->next;
+		free(temp);
+		return (1);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	while (i < index - 1)
 	{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
 	}
 
-	return (NULL);
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
+	return (1);
 }
