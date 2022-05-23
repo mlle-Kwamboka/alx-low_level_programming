d9ac83db2e017300e0149ad8e9fbc89725419d5c
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-05-23 14:32:18 -0700

    Advanced task 2

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char *s = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+	printf("Size is %lu", strlen(s));
+
+	return (0);
+}
