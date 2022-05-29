62a38f3dec2e5c50181e252ad7bbb71146e4a1ce
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-05-29 03:35:24 -0700

    Add advanced 3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - prints any combination of 2 2-digit
+ * numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <=9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i = j || i > j)
+					{
+						putchar(i +'0');
+						putchar (j +'0');
+						if (i != 9 || j != 8)
+						{
+							putchar (' ');
+							if (k <= j)
+							{
+								putchar(k + '0');
+								putchar(l + '0');
+								if (k != 9 || j != 9)
+								{
+									putchar(',');
+									putchar(' ');
+								}
+							}
+
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
