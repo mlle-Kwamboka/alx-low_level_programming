2bdf186a0e405604ca0b78669c20772749729830
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-05-29 12:28:34 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,13 +19,12 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-			}
-			if (i != 8 && j != 9)
-			{
-				putchar (',');
-				putchar (' ');
-			}
-				
+				if (i != 8; j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}		
 		}
 	}
 	putchar ('\n');
