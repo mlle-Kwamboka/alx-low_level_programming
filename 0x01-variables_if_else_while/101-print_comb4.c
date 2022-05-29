82818e1bf5efc6ce057e74baad5badcf79291fbd
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-05-29 02:56:58 -0700

    Add advanced task 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,8 +9,26 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 7; i++)
 	{
-
+		for (j = 0; j <=8; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 7 || j != 8 || k != 9)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}	
+			}
+		}
 	}
+	putchar('\n');
+	return (0);
 }
