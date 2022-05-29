05e815697814e91533b4104ed32c9ee218ea1e71
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-05-29 12:18:15 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (i != j && j != i && i < j)
 			{
