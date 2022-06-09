f4a4dfa5aed90e82a2f6f07138c10250ea01a988
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-06-09 20:09:58 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -43,6 +43,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 		return (1);
 	}
 
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+
 	temp->prev->next = temp->next;
 	if (temp->next)
 		temp->next->prev = temp->prev;
