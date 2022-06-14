f1d17a509cd75d5f667d8729bb57ddf83a3205ce
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-06-14 06:10:49 -0700

    Add library to be called from python

---
+++ 0x18-dynamic_libraries/libs.h
@@ -0,0 +1,11 @@
+#ifndef LIBS_H
+#define LIBS_H
+
+int add(int num1, int num2);
+int mul(int num1, int num2);
+int sub(int num1, int num2);
+int div(int num1, int num2);
+int mod(int num1, int num2);
+
+#endif
+
